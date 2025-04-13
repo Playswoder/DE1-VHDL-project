04ecf83d8e00cd5c69e03d71a62ebd4f89f7b13d
Playswoder <playswoder.entr@gmail.com>
2025-04-13 17:33:06 +0200

    update switch

--- src/switch.vhdl
+++ src/switch.vhdl
@@ -20,21 +20,44 @@ entity switch is
            fmm      : out STD_LOGIC_VECTOR (5 downto 0);
            fss      : out STD_LOGIC_VECTOR (5 downto 0);
            fvv      : out STD_LOGIC_VECTOR (6 downto 0);
-           mode     : out STD_LOGIC_VECTOR (1 downto 0));
+           mode     : out STD_LOGIC_VECTOR (1 downto 0)
+           );
            
 end switch;
 
 architecture Behavioral of switch is
     
+signal sig_mode : unsigned(1 downto 0) := "00";
+
+
+
 begin
-    process(hh, mm, ss, D, ahh, amm, smm, sss, svv)
+ -- pokud to bude dělat latche tak to uděláme synchronní
+   Mode_Sel : process(D)
+   begin
+            if mode = "10" then
+                D_prev <= '0';
+            else
+                D_prev <= D; -- Edge detection
+                    if D = '1' and D_prev = '0' then
+                        sig_mode = sig_mode + 1;
+                        else
+                        sig_mode <= '0'; 
+                    end if;
+                end if;
+
+   end process Mode_Sel;
+    
+    mode <= std_logic_vector(sig_mode);
+
+    Switch_proc : process(hh, mm, ss, D, ahh, amm, smm, sss, svv)
     begin
         mode <= "00";
         fhh <= (others => '0');
         fmm <= (others => '0');
         fss <= (others => '0');
         fvv <= (others => '0');
-        case D is
+        case std_logic_vector(sig_mode) is
             when "00" =>
                 fhh <= hh;
                 fmm <= mm;
@@ -53,5 +76,5 @@ begin
             when others =>
                 mode <= "00";
         end case;
-    end process;
+    end process Switch_proc;
 end Behavioral;
